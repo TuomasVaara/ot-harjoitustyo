ec412a2631a429f9adc37ee6cf3af9e9a703b971
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2021-11-10 08:52:42 +0200

    Viikon2 Poetry harjoittelua

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,36 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+
+    def setUp(self):
+        self.kortti = Maksukortti(10)
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6 euroa")
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+        # nyt kortin saldo on 2
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.kortti.lataa_rahaa(25)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 35 euroa")
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.kortti.lataa_rahaa(200)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150 euroa")
