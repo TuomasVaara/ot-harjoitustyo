7a45e08fa04ba1654cce6a1e0cec24dd0e582ef9
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2021-11-18 16:31:41 +0200

    data

--- src/repositories/Expenses_repository.py
+++
@@ -1,4 +0,0 @@
-
-class ExpensesRepository:
-    self __init__(self):
-    None
--- src/repositories/expenses_repository.py
+++ src/repositories/expenses_repository.py
@@ -1,19 +1,19 @@
 import json
-class income_repository:
+class expenses_repository:
 
     def __init__ (self):
         self.list = []
-        self.path = "ot-harjoitustyo/src/databases/i_data.json"
+        self.path = "ot-harjoitustyo/src/databases/e_data.json"
 
     def load_file(self):
         with open(self.path) as file:
             self.list = json.load(file)
     
-    def add_income(self, expense, amount):
+    def add_expenses(self, expense, amount):
         self.load_file()
         self.list[expense] = amount
         with open(self.path, "w") as file:
             json.dump(self.list, file)
 
 if __name__ == "__main__":
-    f = income_repository()
+    f = expenses_repository()
