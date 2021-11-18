74cd294ab833aecc38c5e596ee34ca6edf952bb8
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2021-11-18 14:17:55 +0200

    dataa

--- src/operations/budjet_operations.py
+++ src/operations/budjet_operations.py
@@ -1,8 +1,9 @@
 from entities.budjet import Budjet
 from entities.user import User 
+import json
 
-from repositories.Expenses_repository import ExpensesRepository
-from repositories.Income_repository import IncomeRepository
+from repositories.Expenses_repository import ExpensesReporitory
+from repositories.Income_reporitory import IncomeRepository
 
 class BudjetOperations:
     def self __init__(self, expenses_repository, income_repository):
@@ -10,8 +11,14 @@ class BudjetOperations:
         self._income_repository = IncomeRepository
         self._user = None
 
-    def add_expenses(self, expense, amount): None
+    def add_expenses(self, expense:str, amount:int): 
+        exp = {"expense" : expense, "amount" = amount}
+        with open("expenses_data.json", "w") as outfile:
+            json.dump(exp, outfile)
 
     def add_income(self, income, amount): None
 
     def show_summary(self): None
+
+if __name__ == "__main__":
+    add_expenses("Rent",587)
